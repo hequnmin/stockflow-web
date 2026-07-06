97dfb9761368de9965f328111c8daca49b250851
Min Ho <hequnmin@gmail.com>
2026-07-06 14:50:07 +0800

    Check TypeScript.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  let title: string = 'StockFlow';
+</script>
+
+<h1>{title}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
